14493e21ca085823d517bc5e5e9797583226d2bc
ZakiAbuKhaled <tllatttt667@gmail.com>
2021-11-21 18:53:16 +0200

    first commit

---
+++ app/src/main/java/com/example/bmiapp/activity_BMI_Login.java
@@ -0,0 +1,13 @@
+package com.example.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_BMI_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmi_login);
+    }
+}
