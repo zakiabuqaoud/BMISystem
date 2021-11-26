342010ea3848480944b4eb4c2ff09d2b4b2561ff
ZakiAbuKhaled <tllatttt667@gmail.com>
2021-11-26 23:59:02 +0200

    add complete your information and home activity

---
+++ app/src/main/java/com/example/bmiapp/activity_complete_your_information.java
@@ -0,0 +1,14 @@
+package com.example.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_complete_your_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_your_information);
+    }
+}
