dab0080f07075391f7d6f0ddb31b0cb0ca675c05
ZakiAbuKhaled <tllatttt667@gmail.com>
2021-12-03 22:25:15 +0200

    third commit

--- app/src/main/java/com/example/bmiapp/MainActivity.java
+++ app/src/main/java/com/example/bmiapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bmiapp;
 
+import android.os.CountDownTimer;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int counter = 5000;
+        CountDownTimer countDownTimer;
+        countDownTimer = new CountDownTimer(counter,1000) {
+            public void onTick(long millisUntilFinished){}
+            public  void onFinish(){ }
+        }.start();
+        Intent intent = new Intent(MainActivity.this,activity_BMI_Login.class);
+        this.finish();
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/bmiapp/activity_new_record.java
@@ -0,0 +1,14 @@
+package com.example.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_new_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+    }
+}
