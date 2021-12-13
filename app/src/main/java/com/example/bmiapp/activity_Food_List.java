47dc90ccaa8bc907801f837d859321eac87fd8f8
ZakiAbuKhaled <tllatttt667@gmail.com>
2021-12-13 21:38:49 +0200

    Add Edit Food Details

---
+++ app/src/main/java/com/example/bmiapp/activity_Food_List.java
@@ -0,0 +1,14 @@
+package com.example.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_Food_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+    }
+}
---
+++ app/src/main/java/com/example/bmiapp/activity_edit_food_details.java
@@ -0,0 +1,14 @@
+package com.example.bmiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_edit_food_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_food_details);
+    }
+}
